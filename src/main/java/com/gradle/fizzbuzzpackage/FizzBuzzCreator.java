bc95ffd0f1fc6613b08974fa6e30f8082adb11d6
EWright212 <emilywright212@gmail.com>
2020-09-17 11:17:58 +0100

    Refactored, for integers the if statement is more efficient

--- src/main/java/com/gradle/fizzbuzzpackage/FizzBuzzCreator.java
+++ src/main/java/com/gradle/fizzbuzzpackage/FizzBuzzCreator.java
@@ -5,13 +5,13 @@ public class FizzBuzzCreator {
     public String generate(int i) {
 
         String output = new String("");
-        if (i % 15 == 0) {
-            output +=  "FizzBuzz";
-        } else if (i % 3 == 0) {
+        if (i % 3 == 0) {
             output += "Fizz";
-        } else if (i % 5 == 0) {
+        }
+        if (i % 5 == 0) {
             output += "Buzz";
-        } else {
+        }
+        if ( output.isEmpty() ){
             output = Integer.toString(i);
         }
         return output;
